e3a2704942353d6a63c79b4693f1d33ece9941e4
PROMURALI77 <84012752+PROMURALI77@users.noreply.github.com>
2022-03-12 16:29:18 +0530

    Add files via upload

---
+++ commands/Coinpayment/_setup.js
@@ -0,0 +1,14 @@
+/*CMD
+  command: /setup
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: Coinpayment
+  answer: 😁
+  keyboard: 
+  aliases: 
+CMD*/
+
+Libs.CoinPayments.setPrivateKey('F69f3d0DCc91df02ec735C2b93A8B0BfC6e96675D4Eb1EF0b877b0e1fbFfF510');
+Libs.CoinPayments.setPublicKey('a48cd6942ad7396d7845d2f90a7eb614827caef482b0e75f6e448158df58788c');
+Libs.CoinPayments.setBBApiKey('tfomtXY8xMcpuEdmzh1NWOxMdyKXEhcPD8vqfj0a');
